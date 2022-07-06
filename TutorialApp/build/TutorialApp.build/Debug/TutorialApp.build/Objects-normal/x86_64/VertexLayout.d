ee0ea4ed3c9bb0b9430aa6d8f429db26275441f3
MitIoanAdrian <62372981+MitIoanAdrian@users.noreply.github.com>
2022-07-06 17:49:32 +0300

    Delete TutorialApp/build directory

--- TutorialApp/build/TutorialApp.build/Debug/TutorialApp.build/Objects-normal/x86_64/VertexLayout.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/adrianm2/Desktop/OpenGLWindow/OpenGLWindow/TutorialApp/src/VertexLayout.cpp \
-  /Users/adrianm2/Desktop/OpenGLWindow/OpenGLWindow/TutorialApp/include/VertexLayout.h \
-  /Users/adrianm2/Desktop/OpenGLWindow/OpenGLWindow/TutorialApp/include/OGL.h \
-  /usr/local/Cellar/glfw/3.3.7/include/GLFW/glfw3.h
--- TutorialApp/build/TutorialApp.build/Debug/TutorialApp.build/Objects-normal/x86_64/main.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/adrianm2/Desktop/OpenGLWindow/OpenGLWindow/TutorialApp/src/main.cpp \
-  /Users/adrianm2/Desktop/OpenGLWindow/OpenGLWindow/TutorialApp/include/Application.hpp \
-  /Users/adrianm2/Desktop/OpenGLWindow/OpenGLWindow/TutorialApp/include/OGL.h \
-  /usr/local/Cellar/glfw/3.3.7/include/GLFW/glfw3.h
