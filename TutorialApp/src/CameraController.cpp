6956a80fff45a1a0e4ef424229edb496987a08cc
MitIoanAdrian <mit.adrian@yahoo.com>
2022-07-19 12:42:57 +0300

    fixed InitPersProjTransform in math_3d.cpp

--- TutorialApp/src/CameraController.cpp
+++ TutorialApp/src/CameraController.cpp
@@ -1,4 +1,29 @@
 #include <CameraController.h>
 #include <OGL.h>
 
-void CameraController::moveCamera(int key, Camera camera) {}
+void CameraController::moveCamera(int key, Camera camera) {
+    Vector3f move = Vector3f(0.0f, 0.0f, 0.0f);
+    
+    switch(key){
+        case GLFW_KEY_UP:
+            move.z -= 1.0f;
+            break;
+        
+        case GLFW_KEY_DOWN:
+            move.z += 1.0f;
+            break;
+        
+        case GLFW_KEY_LEFT:
+            move.x -= 1.0f;
+            break;
+            
+        case GLFW_KEY_RIGHT:
+            move.x += 1.0f;
+            break;
+            
+     
+    }
+    
+    camera.Move(move);
+    
+}
